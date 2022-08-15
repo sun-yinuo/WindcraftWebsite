a8c467ec146696ea104699c3cf36457f868f682c
孙一诺 <11404164+sun-yi-nuo@user.noreply.gitee.com>
2022-08-15 21:10:09 +0800

    springboot init

---
+++ windcraft-backend/src/main/java/com/sunyinuo/windcraftbackend/WindcraftBackendApplication.java
@@ -0,0 +1,17 @@
+package com.sunyinuo.windcraftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主类
+ * @author martin
+ */
+@SpringBootApplication
+public class WindcraftBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindcraftBackendApplication.class, args);
+    }
+
+}
---
+++ windcraft-backend/src/test/java/com/sunyinuo/windcraftbackend/WindcraftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyinuo.windcraftbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindcraftBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
